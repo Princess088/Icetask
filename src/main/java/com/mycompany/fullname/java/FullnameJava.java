a200dc6c831bf597da6d9280279712e91a8dffe3
RC_Student_lab <RC_Student_lab@labG9AEB3.phdvvtwzx4oudioi2mg3lhtmwg.fx.internal.cloudapp.net>
2024-10-11 11:00:57 +0000

    first commit

---
+++ src/main/java/com/mycompany/fullname/java/FullnameJava.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fullname.java;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class FullnameJava {
+
+    public static void main(String[] args) {
+           
+         // Declarations and information
+        String fullName = "Princess Ncedo Nkosingiphile Dladla";
+        String emailAddress = "princessdladla1@gmail.com";
+        String phoneNumber = "0638203094";
+        
+        // Print on a separate lines
+        System.out.println(fullName);
+        System.out.println(emailAddress);
+        System.out.println(phoneNumber);
+    }
+
+    }
